9fad40f491196305199330aa40894cb0f265b617
mohan3d <mohan3d94@gmail.com>
2019-06-19 07:41:49 +0800

    feat: add client features definition

--- openload/client.go
+++ openload/client.go
@@ -36,12 +36,102 @@ type Client struct {
 
 // AccountInfo requests logged-in account info
 // And returns AccountInfo object holding infos.
-func (c *Client) AccountInfo() (*AccountInfo, error) {
-	var info AccountInfo
-	if err := c.get("/account/info", nil, &info); err != nil {
-		return nil, err
-	}
-	return &info, nil
+// https://openload.co/api#accountinfos
+func (c *Client) AccountInfo() (*AccountInfoResponse, error) {
+	return nil, nil
+}
+
+// DownloadTicket requests download ticket for a specific file
+// This ticket will be used for actual download.
+// https://openload.co/api#download-ticket
+func (c *Client) DownloadTicket(fileID string) (*DownloadTicketResponse, error) {
+	return nil, nil
+}
+
+// DownloadLink requests direct download link for a specific file
+// This step must be executed after getting ticket and captcha response
+// From previous step (DownloadTicket).
+// https://openload.co/api#download-getlink
+func (c *Client) DownloadLink(fileID string, ticket string, captchaResponse string) (*DownloadLinkResponse, error) {
+	return nil, nil
+}
+
+// FileInfo requests a single file info.
+// https://openload.co/api#download-info
+func (c *Client) FileInfo(fileID string) (*FileInfoResponse, error) {
+	return nil, nil
+}
+
+// FilesInfo requests info for a list of files.
+// https://openload.co/api#download-info
+func (c *Client) FilesInfo(filesID []string) (*FilesInfoResponse, error) {
+	return nil, nil
+}
+
+// UploadLink requests an upload URL
+// This URL will be used to perform actual upload.
+// https://openload.co/api#upload
+func (c *Client) UploadLink(folderID string, sha1 string, httponly bool) (*UploadURLResponse, error) {
+	return nil, nil
+}
+
+// TODO: Upload.
+
+// RemoteUpload adds a remote upload.
+// https://openload.co/api#remoteul-add
+func (c *Client) RemoteUpload(url string, folderID string) (*RemoteUploadResponse, error) {
+	return nil, nil
+}
+
+// RemoteUploadStatus checks status of remote upload requests.
+// https://openload.co/api#remoteul-check
+func (c *Client) RemoteUploadStatus(limit int64, uploadID string) (RemoteUploadsStatusResponse, error) {
+	return nil, nil
+}
+
+// ListFolder lists a folder content.
+// https://openload.co/api#file-listfolder
+func (c *Client) ListFolder(folderID string) (*ListFolderResponse, error) {
+	return nil, nil
+}
+
+// RenameFolder renames existing folder.
+// https://openload.co/api#file-renamefolder
+func (c *Client) RenameFolder(folderID string, name string) (RenameFolderResponse, error) {
+	return nil, nil
+}
+
+// RenameFile renames existing file.
+// https://openload.co/api#file-rename
+func (c *Client) RenameFile(fileID string, name string) (RenameFolderResponse, error) {
+	return nil, nil
+}
+
+// DeleteFile deletes existing file.
+// https://openload.co/api#file-delete
+func (c *Client) DeleteFile(fileID string) (DeleteFileResponse, error) {
+	return nil, nil
+}
+
+// ConvertFile asks openload to convert media file.
+// Usually conversions settings should be set in the UI
+// https://openload.co/account#conversionsettings
+// https://openload.co/api#convertingfiles
+func (c *Client) ConvertFile(fileID string) (ConvertFileResponse, error) {
+	return nil, nil
+}
+
+// RunningConversions checks pending conversions status.
+// https://openload.co/api#file-runningconverts
+func (c *Client) RunningConversions(folderID string) (RunningConversionsResponse, error) {
+	return nil, nil
+}
+
+// SplashImage request splash image direct url
+// Usually it should be used with media fileID (movie, ...)
+// https://openload.co/api#file-splash
+func (c *Client) SplashImage(fileID string) (SplashImageResponse, error) {
+	return nil, nil
 }
 
 func (c *Client) getAPIURL(p string, q map[string]string) (string, error) {
@@ -85,6 +175,8 @@ func processResponse(response io.Reader, result interface{}) error {
 	if err != nil {
 		return err
 	}
+	// fmt.Println("#$@@#$@#$@$#")
+	// fmt.Println(string(*data["result"]))
 	return json.Unmarshal(*data["result"], &result)
 }
 
